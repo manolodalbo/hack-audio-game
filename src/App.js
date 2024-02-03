1442299fbac908f59343faab7c92523b0125bd0e
manolodalbo <manuel_dal_bo@brown.edu>
2024-02-03 15:18:38 -0500

    starting

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.css';
+import { useRef, useState } from "react";
+
+let animationController;
 
 function App() {
+  const [file, setFile] = useState(null);
+  const canvasRef = useRef();
+  const audioRef = useRef();
+  const source = useRef();
+  const analyzer = useRef();
+
+  const handleAudioPlay = () => {};
+  const visualizeData = () => {};
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input
+        type="file"
+        onChange={({ target: { files } }) => files[0] && setFile(files[0])}
+      />
+      {/* <header className="App-header">
+       <p>Select A Song:</p>
+      </header> */}
+      {file && (
+        <audio
+          ref={audioRef}
+          onPlay={handleAudioPlay}
+          src={window.URL.createObjectURL(file)}
+          controls
+        />
+      )}
+      <canvas ref={canvasRef} width={500} height={200} />
     </div>
   );
 }
-
 export default App;
